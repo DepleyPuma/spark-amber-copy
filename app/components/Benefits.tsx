f844600a1518f3f84ad69c99df65bec4c1ba0f74
DepleyPuma <szymon.szarek1@wp.pl>
2025-05-27 16:47:20 +0200

    create top banner

--- app/components/Benefits.tsx
+++ app/components/Benefits.tsx
@@ -1,6 +1,7 @@
 export function Benefits() {
 	return (
-		<section className='py-24 mx-auto flex flex-col justify-center items-center gap-10 bg-(--bg-pink) lg:gap-14'>
+		<section className='relative py-24 mx-auto flex flex-col justify-center items-center gap-10 bg-(--bg-pink) lg:gap-14'>
+			<div className='absolute -top-[20%] bg-(--bg-pink) h-40 rotate-3 w-[110%] -z-10 '></div>
 			<h1 className='text-white text-6xl font-[700]'>Benefits</h1>
 		</section>
 	);
