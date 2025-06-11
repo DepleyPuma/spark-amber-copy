fcda9ec7096b68d6f6dd06fe688133f49f988405
DepleyPuma <szymon.szarek1@wp.pl>
2025-06-11 17:19:31 +0200

    change card properties

--- app/components/PricingSection.tsx
+++ app/components/PricingSection.tsx
@@ -7,13 +7,14 @@ import { CreditCard } from 'lucide-react';
 
 export function PricingSection() {
 	return (
-		<section className='relative py-24 mx-auto max-w-3xl flex flex-col justify-center items-center gap-10 lg:gap-14'>
+		<section
+			className='relative py-24 mx-auto max-w-3xl flex flex-col justify-center items-center gap-10 lg:gap-14'
+			id='pricing'>
 			<AnimatedText
 				as='h1'
 				splitBy='words'
 				delay={0.5}
-				className='text-4xl md:text-5xl font-black font-recursive text-center text-(--dark-text)'
-			>
+				className='text-4xl md:text-5xl font-black font-recursive text-center text-(--dark-text)'>
 				Find the Perfect Plan for YOU
 			</AnimatedText>
 
@@ -21,24 +22,18 @@ export function PricingSection() {
 				<Card className='w-full max-w-md bg-(--white-text)'>
 					<CardHeader>
 						<CardTitle>Basic</CardTitle>
-						<CardDescription>
-							The essential tools and filters to generate SaaS ideas.
-						</CardDescription>
+						<CardDescription>The essential tools and filters to generate SaaS ideas.</CardDescription>
 					</CardHeader>
 					<CardContent>
 						<h3 className='flex justify-start items-end gap-1 text-5xl font-extrabold'>
-							<span className='text-lg text-gray-500 font-[600] line-through'>
-								$35
-							</span>
+							<span className='text-lg text-gray-500 font-[600] line-through'>$35</span>
 							<span>$15</span>
 							<span className='text-base font-[600]'>USD</span>
 						</h3>
 
 						<ul className='mt-8 space-y-3 text-xl'>
 							<li className="relative pl-6 text-sm before:content-['✓']  before:absolute before:left-0 before:text-(--pink-text) before:font-bold opacity-75">
-								<span className='text-(--dark-text)'>
-									5 idea generations per day
-								</span>
+								<span className='text-(--dark-text)'>5 idea generations per day</span>
 							</li>
 							<li className="relative pl-6 text-sm before:content-['✓']  before:absolute before:left-0 before:text-(--pink-text) before:font-bold opacity-75">
 								<span className='text-(--dark-text)'>All categories</span>
@@ -52,20 +47,16 @@ export function PricingSection() {
 							<li className="relative pl-6 text-sm before:content-['✓']  before:absolute before:left-0 before:text-(--pink-text) before:font-bold opacity-75">
 								<span className='text-(--dark-text)'>Custom category prompts</span>
 							</li>
-							<li className="relative pl-6 text-sm before:content-['x'] before:absolute before:left-0 before:text-(--dark-text) before:font-bold opacity-75">
-								<span className='text-(--dark-text)'>
-									ChatGPT Prompts for coding generated ideas
-								</span>
+							<li className="relative pl-6 text-sm before:content-['x'] before:absolute before:left-0 before:text-(--dark-text) before:font-bold opacity-50">
+								<span className='text-(--dark-text)'>ChatGPT Prompts for coding generated ideas</span>
 							</li>
-							<li className="relative pl-6 text-sm before:content-['x'] before:absolute before:left-0 before:text-(--dark-text) before:font-bold opacity-75">
-								<span className='text-(--dark-text)'>
-									Step-by-step guide for coding ideas
-								</span>
+							<li className="relative pl-6 text-sm before:content-['x'] before:absolute before:left-0 before:text-(--dark-text) before:font-bold opacity-50">
+								<span className='text-(--dark-text)'>Step-by-step guide for coding ideas</span>
 							</li>
-							<li className="relative pl-6 text-sm before:content-['x'] before:absolute before:left-0 before:text-(--dark-text) before:font-bold opacity-75">
+							<li className="relative pl-6 text-sm before:content-['x'] before:absolute before:left-0 before:text-(--dark-text) before:font-bold opacity-50">
 								<span className='text-(--dark-text)'>Business model proposal</span>
 							</li>
-							<li className="relative pl-6 text-sm before:content-['x'] before:absolute before:left-0 before:text-(--dark-text) before:font-bold opacity-75">
+							<li className="relative pl-6 text-sm before:content-['x'] before:absolute before:left-0 before:text-(--dark-text) before:font-bold opacity-50">
 								<span className='text-(--dark-text)'>Lifetime updates</span>
 							</li>
 						</ul>
@@ -74,33 +65,25 @@ export function PricingSection() {
 						<Button className='flex justify-center items-center w-full gap-2 rounded-xl'>
 							Buy <CreditCard />
 						</Button>
-						<p className='text-xs text-(--dark-text) opacity-75'>
-							Pay once. Acces forever.
-						</p>
+						<p className='text-xs text-(--dark-text) opacity-75'>Pay once. Acces forever.</p>
 					</CardFooter>
 				</Card>
 
 				<Card className='w-full max-w-md bg-(--white-text)'>
 					<CardHeader>
 						<CardTitle>Basic</CardTitle>
-						<CardDescription>
-							The essential tools and filters to generate SaaS ideas.
-						</CardDescription>
+						<CardDescription>The essential tools and filters to generate SaaS ideas.</CardDescription>
 					</CardHeader>
 					<CardContent>
 						<h3 className='flex justify-start items-end gap-1 text-5xl font-extrabold'>
-							<span className='text-lg text-gray-500 font-[600] line-through'>
-								$100
-							</span>
+							<span className='text-lg text-gray-500 font-[600] line-through'>$100</span>
 							<span>$49</span>
 							<span className='text-base font-[600]'>USD</span>
 						</h3>
 
 						<ul className='mt-8 space-y-3 text-xl'>
 							<li className="relative pl-6 text-sm before:content-['✓']  before:absolute before:left-0 before:text-(--pink-text) before:font-bold opacity-75">
-								<span className='text-(--dark-text)'>
-									Unlimited idea generations
-								</span>
+								<span className='text-(--dark-text)'>Unlimited idea generations</span>
 							</li>
 							<li className="relative pl-6 text-sm before:content-['✓']  before:absolute before:left-0 before:text-(--pink-text) before:font-bold opacity-75">
 								<span className='text-(--dark-text)'>All categories</span>
@@ -115,14 +98,10 @@ export function PricingSection() {
 								<span className='text-(--dark-text)'>Custom category prompts</span>
 							</li>
 							<li className="relative pl-6 text-sm before:content-['✓'] before:absolute before:left-0 before:text-(--pink-text) before:font-bold opacity-75">
-								<span className='text-(--dark-text)'>
-									ChatGPT Prompts for coding generated ideas
-								</span>
+								<span className='text-(--dark-text)'>ChatGPT Prompts for coding generated ideas</span>
 							</li>
 							<li className="relative pl-6 text-sm before:content-['✓'] before:absolute before:left-0 before:text-(--pink-text) before:font-bold opacity-75">
-								<span className='text-(--dark-text)'>
-									Step-by-step guide for coding ideas
-								</span>
+								<span className='text-(--dark-text)'>Step-by-step guide for coding ideas</span>
 							</li>
 							<li className="relative pl-6 text-sm before:content-['✓'] before:absolute before:left-0 before:text-(--pink-text) before:font-bold opacity-75">
 								<span className='text-(--dark-text)'>Business model proposal</span>
@@ -136,9 +115,7 @@ export function PricingSection() {
 						<Button className='flex justify-center items-center w-full gap-2 rounded-xl'>
 							Buy <CreditCard />
 						</Button>
-						<p className='text-xs text-(--dark-text) opacity-75'>
-							Pay once. Acces forever.
-						</p>
+						<p className='text-xs text-(--dark-text) opacity-75'>Pay once. Acces forever.</p>
 					</CardFooter>
 				</Card>
 			</div>
